3bd1f1533c6003093f03c7e56b70fafcba140811
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-09-01 11:11:57 -0300

    fix/ commented transactionsMock array

--- src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,18 +1,18 @@
 import { useDashboard } from '../../DashboardContext/useDashboardContext';
-const transacionsMock = [
-  {
-    type: 'expense',
-    name: 'Almoço',
-    value: 24,
-    date: '22/02/2023',
-  },
-  {
-    type: 'income',
-    name: 'Salario',
-    value: 1679.2,
-    date: '22/02/2023',
-  },
-];
+// const transacionsMock = [
+//   {
+//     type: 'expense',
+//     name: 'Almoço',
+//     value: 24,
+//     date: '22/02/2023',
+//   },
+//   {
+//     type: 'income',
+//     name: 'Salario',
+//     value: 1679.2,
+//     date: '22/02/2023',
+//   },
+// ];
 export function useTransactionsController() {
   const { areValuesVisible } = useDashboard();
   return {
